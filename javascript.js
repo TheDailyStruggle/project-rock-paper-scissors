18479dc1ec9c3ff7d2d1e6a166aa1ce611085d81
TheDailyStruggle <chadmontgomery46@gmail.com>
2023-02-25 16:28:51 -0800

    Add functionality for 5 round game

--- javascript.js
+++ javascript.js
@@ -1,18 +1,30 @@
-// Check if user wins or loses
-// Display alert won or loss
-
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        console.log("It's a tie! Play again!")
-    } else if (playerSelection !== computerSelection) {
-        console.log("Test Game");
+        console.log("It's a tie! Play again!");
+        return result = 'tie';
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        console.log("You lose :( Paper covers rock!");
+        return result = 'lose';
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        console.log("You Win! Rock smashes scissors!");
+        return result = 'win';
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        console.log("You Win! Paper covers rock!");
+        return result = 'win';
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        console.log("You Lose :( Scissors cut paper!");
+        return result = 'lose';
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        console.log("You Lose :( Rock smashes scissors!");
+        return result = 'lose';
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        console.log("You Win! Scissors cut paper!");
+        return result = 'win';
     }
 }
 
-
-
-const playerSelection = playerInput();
-const computerSelection = getComputerChoice();
+let playerScore = 0;
+let computerScore = 0;
 
 function playerInput() {
     input = prompt("Rock, Paper, or Scissors");
@@ -27,7 +39,29 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-playRound(playerSelection, computerSelection);
+function game() {
+    for (i = 0; i < 5; i++) {
+        const playerSelection = playerInput();
+        const computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+        if (result === 'tie') {
+            console.log(`Computer: ${computerScore} You: ${playerScore}`)
+        } else if (result === 'win') {
+            playerScore++;
+            console.log(`Computer: ${computerScore} You: ${playerScore}`)
+        } else if (result === 'lose') {
+            computerScore++;
+            console.log(`Computer: ${computerScore} You: ${playerScore}`)
+        }
+    }
+    if (playerScore === computerScore) {
+        console.log("After 5 rounds you decided nothing. It's a tie")
+    } else if (playerScore > computerScore) {
+        console.log(`That's the game. You've conquered the computer ${playerScore} to ${computerScore}`)
+    } else if (computerScore > playerScore) {
+        console.log(` That's the game. You've lost to the computer ${computerScore} to ${playerScore}`)
+    }
+}
 
-console.log(1, playerSelection);
-console.log(2, computerSelection);
+
+game();
