c3873cfa6b70e25b89b71fa115da4d9afbf816ca
TheDailyStruggle <chadmontgomery46@gmail.com>
2023-03-17 23:45:33 -0700

    Add first bits of UI to RPS

--- javascript.js
+++ javascript.js
@@ -1,3 +1,35 @@
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorsBtn = document.querySelector('#scissors');
+
+let playerSelection = "0";
+
+rockBtn.addEventListener('click', () => {
+    playerSelection = "rock"
+    computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+    if (result === "win") {
+        playerScore++;
+    } else if (result === "lose") {
+        computerScore++;
+    } else {
+        alert("It's a tie!");
+    }
+})
+
+paperBtn.addEventListener('click', () => {
+    playerSelection = "paper"
+    computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+})
+
+scissorsBtn.addEventListener('click', () => {
+    playerSelection = "scissors"
+    computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+})
+
+
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         console.log("It's a tie! Play again!");
@@ -26,11 +58,11 @@ function playRound(playerSelection, computerSelection) {
 let playerScore = 0;
 let computerScore = 0;
 
-function playerInput() {
-    input = prompt("Rock, Paper, or Scissors");
-    choice = input.toLowerCase();
-    return choice;
-}
+// function playerInput() {
+//     input = prompt("Rock, Paper, or Scissors");
+//     choice = input.toLowerCase();
+//     return choice;
+// }
 
 function getComputerChoice() {
     options = ["rock", "paper", "scissors"];
@@ -39,29 +71,27 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-function game() {
-    for (i = 0; i < 5; i++) {
-        const playerSelection = playerInput();
-        const computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection);
-        if (result === 'tie') {
-            console.log(`Computer: ${computerScore} You: ${playerScore}`)
-        } else if (result === 'win') {
-            playerScore++;
-            console.log(`Computer: ${computerScore} You: ${playerScore}`)
-        } else if (result === 'lose') {
-            computerScore++;
-            console.log(`Computer: ${computerScore} You: ${playerScore}`)
-        }
-    }
-    if (playerScore === computerScore) {
-        console.log("After 5 rounds you decided nothing. It's a tie")
-    } else if (playerScore > computerScore) {
-        console.log(`That's the game. You've conquered the computer ${playerScore} to ${computerScore}`)
-    } else if (computerScore > playerScore) {
-        console.log(` That's the game. You've lost to the computer ${computerScore} to ${playerScore}`)
-    }
-}
+// function game() {
+//     for (i = 0; i < 5; i++) {
+//         const computerSelection = getComputerChoice();
+//         playRound(playerSelection, computerSelection);
+//         if (result === 'tie') {
+//             console.log(`Computer: ${computerScore} You: ${playerScore}`)
+//         } else if (result === 'win') {
+//             playerScore++;
+//             console.log(`Computer: ${computerScore} You: ${playerScore}`)
+//         } else if (result === 'lose') {
+//             computerScore++;
+//             console.log(`Computer: ${computerScore} You: ${playerScore}`)
+//         }
+//     }
+//     if (playerScore === computerScore) {
+//         alert("After 5 rounds you decided nothing. It's a tie")
+//     } else if (playerScore > computerScore) {
+//         alert(`That's the game. You've conquered the computer ${playerScore} to ${computerScore}`)
+//     } else if (computerScore > playerScore) {
+//         alert(` That's the game. You've lost to the computer ${computerScore} to ${playerScore}`)
+//     }
+// }
 
-
-game();
+// game();
